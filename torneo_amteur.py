1646c6227918beaeca6fea3a6a5e6bf46214efcc
nicflores2001-cpu <nicflores2001@gmail.com>
2026-06-14 20:47:00 -0300

    Update torneo_amteur.py

--- torneo_amteur.py
+++ torneo_amteur.py
@@ -1,6 +1,8 @@
 # SISTEMA DE GESTIÓN DE TORNEO AMATEUR
 # Trabajo Final Integrador
-
+#El sistema deberá permitir administrar un torneo deportivo amateur. La solución podrá contemplar registro de equipos, carga de resultados,
+#cálculo automático de puntos, posiciones y estadísticas generales del torneo.
+#El sistema podrá incluir control de partidos jugados, diferencia de goles y generación de tablas de posiciones básicas.
 """
 Implementar:
  - estructuras condicionales
@@ -10,13 +12,21 @@ Implementar:
  - acumuladores y contadores
  - manejo básico de errores y mensajes al usuario.
 """
-
-# Funciones:
+# Funciones y procedimientos :
     # Menu
-    # registrar equipos 
-    # carga de resultados
+    # registrar equipos (contemplar carga de varios equipos de manera manual o un archivo)
+    # carga de resultados (Local y visitante)
     # cálculo automático de puntos
     # posiciones
-    # estadísticas generales del torneo
+    # estadísticas generales del torneo (contemplar diferencia de goles, empates,) 
+    # Editar equipos (eliminar, editar nombre )
+    # Premios (habilitado una vez jugado todo los partidos, aparecen solo las primeras 3 posiciones)
+    # Cantidad de partidos: cantidad de equipos - 1 (contemplamos un torneo amateur 1 solo partido de ida)
+    
 
 # mensajes de error
+
+
+
+
+
