c0ffafdefdcf4a4070215ec7f7ca4eb945e4a75d
Yael Garcia <72664493+YaeelG@users.noreply.github.com>
2026-06-14 20:18:34 -0300

    Update torneo_amteur.py

--- torneo_amteur.py
+++ torneo_amteur.py
@@ -1,1 +1,22 @@
+# SISTEMA DE GESTIÓN DE TORNEO AMATEUR
+# Trabajo Final Integrador
 
+"""
+Implementar:
+ - estructuras condicionales
+ - estructuras repetitivas
+ - modularizacion(funciones, def)
+ - valiaciones
+ - acumuladores y contadores
+ - manejo básico de errores y mensajes al usuario.
+"""
+
+# Funciones:
+    # Menu
+    # registrar equipos 
+    # carga de resultados
+    # cálculo automático de puntos
+    # posiciones
+    # estadísticas generales del torneo
+
+# mensajes de error
